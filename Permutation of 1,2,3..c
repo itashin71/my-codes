27c9e561e689279ddcc7a0697c29db35cadd1b5e
itashin71 <itashin71@gmail.com>
2024-05-30 19:52:09 +0600

    code

--- Permutation of 1,2,3..c
+++ Permutation of 1,2,3..c
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 
 #include <stdio.h>
 int main()
@@ -26,32 +25,3 @@ return 0;
 
 
 }
-=======
-
-#include <stdio.h>
-int main()
-{
-
-    int a,b,c;
-    for(a=1;a<=3;a++){
-        for(b=1;b<=3;b++){
-            if(b!=a)
-            {
-                for(c=1;c<=3;c++){
-                        if(c!=a && c!=b)
-                    printf("%d, %d, %d\n",a,b,c);
-                }
-            }
-        }
-    }
-
-
-return 0;
-
-
-
-
-
-
-}
->>>>>>> Stashed changes
