785b8758a790335fa8d58ca7c13a7eefeb332322
itashin71 <itashin71@gmail.com>
2024-05-30 16:32:07 +0600

    code

--- Permutation of 1,2,3..c
+++ Permutation of 1,2,3..c
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 #include <stdio.h>
 int main()
@@ -25,3 +26,32 @@ return 0;
 
 
 }
+=======
+
+#include <stdio.h>
+int main()
+{
+
+    int a,b,c;
+    for(a=1;a<=3;a++){
+        for(b=1;b<=3;b++){
+            if(b!=a)
+            {
+                for(c=1;c<=3;c++){
+                        if(c!=a && c!=b)
+                    printf("%d, %d, %d\n",a,b,c);
+                }
+            }
+        }
+    }
+
+
+return 0;
+
+
+
+
+
+
+}
+>>>>>>> Stashed changes
--- permutation of 1,2,3,4,5.c
+++ permutation of 1,2,3,4,5.c
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #include <stdio.h>
 int main()
 {
@@ -53,3 +54,60 @@ int main()
 
  return 0;
 }
+=======
+#include <stdio.h>
+int main()
+{
+    int a,b,c,d,e;
+    for(a=1;a<=5;a++){
+        for(b=1;b<=5;b++){
+            if(b!=a){
+                for(c=1;c<=5;c++){
+                    if(c!=a && c!=b){
+                        for(d=1;d<=5;d++)
+                        {
+                            if(d!=a && d!=b && d!=c){
+                                for(e=1;e<=5;e++){
+                                    if(e!=a && e!=b && e!=c && e!=d){
+                                        printf("%d, %d, %d, %d, %d\n",a,b,c,d,e);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ return 0;
+}
+>>>>>>> Stashed changes
