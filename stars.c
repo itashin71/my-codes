30f978a8594e666c7a261c1e912580c3e5de4ada
itashin71 <itashin71@gmail.com>
2024-06-02 21:24:28 +0600

    code

---
+++ stars.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("Enter the number of rows you want:  ");
+    scanf("%d",&n);
+    for( i=1; i<=n; i++){
+        for(j=1;j<=2n-1;j++){
+            if(j)
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ type casting.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a=5.0,b=6.0;
+    double avg;
+    avg=(double)(b+a)/2;
+    printf("avg is %0.1lf",avg);
+    return 0;
+
+
+
+
+
+
+}
